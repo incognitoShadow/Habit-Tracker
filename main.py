b27f479b6000406860debede2d426daf251d6aa6
incognitoShadow <redwanhossainminhaj@gmail.com>
2025-12-15 00:43:39 +0600

    Enhance UI design with colors and fonts
    Updated the UI elements with background and font colors for better aesthetics.

--- main.py
+++ main.py
@@ -11,26 +11,27 @@ def Start():
 def yes():
     global frame2, question_entry
     frame1.pack_forget()
-    frame2 = tk.Frame(window)
+    frame2 = tk.Frame(window, bg="#FFFEFA")
     frame2.pack(pady=20)
-    
-    question_label = tk.Label(frame2, text="What should we call you ?")
-    question_entry = tk.Entry(frame2)
+
+    question_label = tk.Label(frame2, text="What should we call you ?", bg="#FFFEFA", fg="#5A5B34", font=("Outfit", 14))
+    question_entry = tk.Entry(frame2, bg = "#F6FFD7")
     question_entry.grid(row=1, column=0, columnspan=2, pady=10)
     question_label.grid(row=0, column=0, columnspan=2, pady=10)
-    
-    Start_btn = tk.Button(frame2, text="Let's Get Started", command=Start)
+
+    Start_btn = tk.Button(frame2, text="Let's Get Started", bg="#5A5B34", fg="#FFFEFA", font=("Outfit", 8), command=Start)
     Start_btn.grid(row=2, column=0, columnspan=2, pady=10)
 
 window = tk.Tk()
 window.title("Habit Tracker Application")
 window.geometry("400x300")
+window.config(bg="#FFFEFA")
 
-frame1 = tk.Frame(window)
+frame1 = tk.Frame(window, bg="#FFFEFA")
 frame1.pack(pady=20)
 
-welcome_Text = tk.Label(frame1, text="Want to gain a new HABIT!?")
-intrested = tk.Button(frame1, text="Yes", command=yes)
+welcome_Text = tk.Label(frame1, text="Want to gain a new HABIT!?", bg="#FFFEFA", fg="#5A5B34", font=("Outfit", 16))
+intrested = tk.Button(frame1, text="Yes", command=yes, bg="#5A5B34", fg="#FFFEFA", font=("Outfit", 8))
 
 welcome_Text.grid(row=0, column=0, columnspan=2, pady=10)
 intrested.grid(row=1, column=0, columnspan=2, pady=10)
