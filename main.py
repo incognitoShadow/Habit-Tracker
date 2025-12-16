63ba6546766026521639d99b5772b1d158304f02
incognitoShadow <redwanhossainminhaj@gmail.com>
2025-12-17 01:55:41 +0600

    Refactor habit tracker UI with ttkbootstrap

--- main.py
+++ main.py
@@ -1,39 +1,90 @@
-import tkinter as tk
+import ttkbootstrap as ttk
 from tkinter import messagebox
-def Start():
+
+def Enter_habits():
+    global frame2, frame3
+    frame2.pack_forget()
+    frame3 = ttk.Frame(window)
+    frame3.pack(expand=True)
+    Enter_habitLabel = ttk.Label(
+        frame3,
+        text="HOW MANY HABITS DO YOU WANT TO TRACK?",
+        font=("Outfit", 14)
+    )
+    Enter_habitLabel.grid(row=0, column=0, columnspan=2, pady=10)
+    Enter_HabitsName=ttk.Entry(
+        frame3,
+        font=("Outfit", 12),
+        width=30
+    )
+    Enter_HabitsName.grid(row=1, column=0,columnspan=2, pady=20)
+def start():
     user_name = question_entry.get()
     if user_name.strip():
-        frame2.pack_forget()
-        messagebox.showinfo("Welcome", f"Welcome, {user_name}! Let's start tracking your habits.")
+        messagebox.showinfo(
+            "consistency is key!",
+            f"Welcome, {user_name}! Let's start tracking your habits."
+        )
     else:
         messagebox.showerror("Error", "Please enter your name!")
+        return frame2.pack()
+    return Enter_habits()    
 
 def yes():
     global frame2, question_entry
-    frame1.pack_forget()
-    frame2 = tk.Frame(window, bg="#FFFEFA")
-    frame2.pack(pady=20)
 
-    question_label = tk.Label(frame2, text="What should we call you ?", bg="#FFFEFA", fg="#5A5B34", font=("Outfit", 14))
-    question_entry = tk.Entry(frame2, bg = "#F6FFD7")
-    question_entry.grid(row=1, column=0, columnspan=2, pady=10)
+    frame1.pack_forget()
+
+    frame2 = ttk.Frame(window)
+    frame2.pack(expand=True)
+
+    question_label = ttk.Label(
+        frame2,
+        text="What should we call you?",
+        font=("Outfit", 14)
+    )
     question_label.grid(row=0, column=0, columnspan=2, pady=10)
 
-    Start_btn = tk.Button(frame2, text="Let's Get Started", bg="#5A5B34", fg="#FFFEFA", font=("Outfit", 8), command=Start)
-    Start_btn.grid(row=2, column=0, columnspan=2, pady=10)
+    question_entry = ttk.Entry(
+        frame2,
+        font=("Outfit", 12),
+        width=25
+    )
+    question_entry.grid(row=1, column=0, columnspan=2, pady=10)
 
-window = tk.Tk()
-window.title("Habit Tracker Application")
-window.geometry("400x300")
-window.config(bg="#FFFEFA")
+    start_btn = ttk.Button(
+        frame2,
+        text="Let's Get Started",
+        command=start,
+        bootstyle="primary-outline",
+        padding=(20, 8),
+        
+    )
+    start_btn.grid(row=2, column=0, columnspan=2, pady=10)
+#main window
+window = ttk.Window(
+    title="Habit Tracker Application",
+    themename="flatly",
+    size=(850, 450)
+)
 
-frame1 = tk.Frame(window, bg="#FFFEFA")
-frame1.pack(pady=20)
+frame1 = ttk.Frame(window)
+frame1.pack(expand=True)
 
-welcome_Text = tk.Label(frame1, text="Want to gain a new HABIT!?", bg="#FFFEFA", fg="#5A5B34", font=("Outfit", 16))
-intrested = tk.Button(frame1, text="Yes", command=yes, bg="#5A5B34", fg="#FFFEFA", font=("Outfit", 8))
+welcome_text = ttk.Label(
+    frame1,
+    text="Want to gain a new HABIT!?",
+    font=("Outfit", 16)
+)
+welcome_text.grid(row=0, column=0, columnspan=2, pady=10)
 
-welcome_Text.grid(row=0, column=0, columnspan=2, pady=10)
-intrested.grid(row=1, column=0, columnspan=2, pady=10)
+interested = ttk.Button(
+    frame1,
+    text="Yes",
+    command=yes,
+    bootstyle="success-outline",
+    padding=(80, 8)
+)
+interested.grid(row=1, column=0, columnspan=2, pady=10)
 
 window.mainloop()
